c652e8860ac42e0888635c3759c409355098585b
LIQIANG777 <LIQIANG777@pingan.com.cn>
2020-03-03 15:23:34 +0800

    phaser初始化

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
-// import serviceWorker from './serviceWorker'
-// serviceWorker()
-console.log(
-  [1, 2, 3].map(function(n) {
-    return n + 1;
-  })
-);
+import Phaser from "phaser";
+var config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
+};
+
+var game = new Phaser.Game(config);
+
+function preload() {}
+
+function create() {}
+
+function update() {}
